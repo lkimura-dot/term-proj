5d4cd17427c06f55e50af812f4f0e37310f14c92
lauren kimura <lkimura@linux10610.dc.engr.scu.edu>
2026-03-06 13:47:54 -0800

    finished comments

--- app2/dataset.c
+++ app2/dataset.c
@@ -2,8 +2,7 @@
  * File: dataset.c
  * Author: Lauren Kimura
  * Last updated: 3/5/2026
- * Description: This 
- *
+ * Description: This file uses an array of students with an ID and age. It is an ADT wih functions createDataset, destroyDataset, Insertion, Deletion, and SearchID
  */
 
 #include <stdlib.h>
@@ -14,7 +13,7 @@
 #include "dataset.h"
 
 /*
- * Defines a student of a certain age with an ID pointing to next and prev
+ * Defines a student with an age and id
  */
 typedef struct student
 {
@@ -23,22 +22,22 @@ typedef struct student
 } STUDENT;
 
 /*
- * Defines an List where the ages are stored, with a max length and current student count.
+ * Defines a list with an array, each slot is pointing to a student. Has a count for how amny student and max length.
  */
 typedef struct list{
-  STUDENT **slp;
+  STUDENT **sp;
   int count;
   int length;
 } LIST;
 
 /*
- * Big-O: O(m) m is the differant types of ages in array
+ * Big-O: O(n) n is the number of max elements in array
  *
- * Average: O(m)
+ * Average: O(n)
  *
- * Worst case: O(m)
+ * Worst case: O(n)
  *
- * The createDataSet function takes in the max array size called maxElts as an  argument. It creates the lp, setting studentCount to 0, the length of maxElts, and distributes memory to the array in lp that size of 13 (18-30). In the lp there is a list of AGES with a dummy head pointer. each node is a STUDENT that is doubly linked. It returns the list lp
+ * The createDataSet funcion takes in the max array size called maxElts as an  argument. It creates the lp, setting max length to maxElts and current count to 0. It gives memory to the lp for sp (student pointer), then gives sp memory to each of those slots. This returns the lp.
  */
 LIST *createDataSet(int maxElts)
 {
@@ -46,55 +45,55 @@ LIST *createDataSet(int maxElts)
     assert(lp != NULL);
 	lp->length = maxElts;
 	lp->count = 0;
-	lp->slp = malloc(sizeof(STUDENT*)*maxElts);
+	lp->sp = malloc(sizeof(STUDENT*)*maxElts);
 	assert(lp != NULL);
 	for(int i = 0; i < maxElts; i++) {
-		lp->slp[i] = malloc(sizeof(STUDENT));
-		assert(lp->slp[i] != NULL);
+		lp->sp[i] = malloc(sizeof(STUDENT));
+		assert(lp->sp[i] != NULL);
 	}
 	return lp;
 }
 
 /*
- * Big-O: O(m), m is the differant types of ages in array
+ * Big-O: O(n), n is the number of max elements in the array
  *
- * Average: O(m)
+ * Average: O(n)
  *
- * Worst case: O(m)
+ * Worst case: O(n)
  *
- * The destroyDataSet function takes in one lq as an argument. It frees each of the age heads and AGES in the array, then frees the array itself, and finally the lp. This returns nothing.
+ * The destroyDataSet function takes in one lp as an argument. It frees each of the points in the sp, then frees the sp itself, then the lp. This returns nothing.
  *
  */
 void destroyDataSet(LIST *lp)
 {
 	assert(lp != NULL);
 	for(int i = 0; i < lp->length; i++) {
-		free(lp->slp[i]); 
+		free(lp->sp[i]); 
 	}
-	free(lp->slp);
+	free(lp->sp);
 	free(lp);
 	printf("Successfully deleted list\n");
 }
 
 /*
- * Big-O: O(m) m is the differant types of ages in array
+ * Big-O: O(1)
+ * 
+ * Average: O(1)
  *
- * Average: O(m), getting age O(1), but returning each ID is O(m)
+ * Worst Case: O(1)
  *
- * Worst Case: O(m)
- *
- * The searchAge function takes one lp and an age as arguments. If there is not student with that age, it returns. Else, it prints out all the student's ID's with that age and reutns nothing
+ * The searchAge function takes one lp and an Id as arguments. If there is not student with that id, it returns -1. Else, it prints the student's ID's and returns the id
  *
  */
 int searchID(LIST *lp, int findId)
 {	
 	assert(lp != NULL);
-	if (lp->slp[findId]->id != findId) {
+	if (lp->sp[findId]->id != findId) {
 		printf("No student with id: %d\n", findId);
 		return -1;
 	}
 	printf("Student %d found!\n", findId);
-	return lp->slp[findId]->id;
+	return lp->sp[findId]->id;
 }
 
 
@@ -105,37 +104,37 @@ int searchID(LIST *lp, int findId)
  *
  * Worst Case:O(1)
  *
- * The insertion function takes in one LIST lp, and int newId and one int newAge as arguments. It creates memory for the new stuent, then sets the students age to the newAge, and well as the ID to newId. It then adds the new student to the beginngin of the double linked list right after the head, incrementing count by 1. It prints out the new student, then returns nothing.
- *
+ * The insertion function takes in one LIST lp, and int newId and one int newAge as arguments. It uses the student Id to insert the new student in that index in lp. It sets that point's id to the newId, the age to the new age, and increments count. This returns nothing
+ * 
  */
 void insertion(LIST *lp, int newId, int newAge)
 {
 	assert(lp != NULL && lp->count < lp ->length);
-	lp->slp[newId]->id = newId;
-	lp->slp[newId]->age = newAge;
+	lp->sp[newId]->id = newId;
+	lp->sp[newId]->age = newAge;
 	printf("New student added with ID: %d\n", newId);
 	lp->count++;
 	return;
 }
 
 /*
- * Big-O: O(m), m is the differant types of ages in array 
+ * Big-O: O(1)
  * 
- * Average: O(m)
+ * Average: O(1)
  *
- * Worst Case: O(n), if all student have the same age, it will delete every STUDENT node
+ * Worst Case: O(1)
  *
- * The deletion function takes in one list of lp and one delAge as arguments. It checks the count of student with that AGE, and if none, returns. If there is a list of STUDENTS, there is a new created STUDENT that points to the first student in the list, and while the next pointer is not the head (reached end) will save the next pointer, then delete the student at first, setting the new student to the next saved. This returns nothing
+ * The deletion function takes in one list of lp and one delId as arguments. It checks if that slot is NULL or if the current Id is not the id intending to delete, and if so returns. Else, it frees that Id point in lp, sets that point to NULL, and decrements count. This returns nothing
  */
 void deletion (LIST *lp, int delId)
 {
 	assert(lp!= NULL);
-	if (lp->slp[delId] == NULL || lp->slp[delId]->id != delId) {
+	if (lp->sp[delId] == NULL || lp->sp[delId]->id != delId) {
 		printf("Student %d could not be deleted, not in list\n", delId);
 		return;
 	}
-	free(lp->slp[delId]);
-	lp->slp[delId] = NULL;
+	free(lp->sp[delId]);
+	lp->sp[delId] = NULL;
 	lp->count--;
 	printf("deleted student %d\n", delId);
 	return;
